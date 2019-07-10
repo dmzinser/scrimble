745a49635bf4efda30f6b3377df4d88e1b0b0e7c
David Zinser <dmzinser@gmail.com>
2019-07-10 14:47:02 -0700

    Create Game Timer

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ inputField.style.display = "none";
 const startButton = document.querySelector("#start");
 startButton.addEventListener("click", () => {
   createPlayer();
-})
+});
 
 createPlayer = () => {
   name = prompt("What is your name?");
@@ -19,6 +19,7 @@ createPlayer = () => {
   inputField.style.display = "block";
   document.querySelector("#input").focus();
   game.shuffleWord(word);
+  game.clock();
 }
 
 class Player {
@@ -27,33 +28,37 @@ class Player {
     this.score = 0;
     this.round = 0;
     this.game = 0;
-  }
+    this.time = 10;
+  };
+
   initPlayerOne() {
-    const playerOneMetrics = document.querySelector("#playerinfo");
     const playerOneName = document.querySelector("#playername");
-    playerOneName.innerText = this.name;
-    playerOneMetrics.appendChild(playerOneName);
     const score = document.querySelector("#score");
     const round = document.querySelector("#round");
+    const time = document.querySelector("#time");
+    playerOneName.innerText = this.name;
     score.innerText = (`Score: ${this.score}`);
     round.innerText = (`Rounds Won: ${this.round}`);
+    time.innerText = (`Time Remaining: ${this.time}`);
   }
 }
+
 const game = {
 
-  score: 0,
-  round: 0,
-
   shuffleWord(word) {
-    wordChars = word.split("");
-    const staticLength = wordChars.length;
-    newWord = [];
-    for(let i = 0; i < staticLength; i++) 
-    newWord += wordChars.splice(Math.floor(Math.random(wordChars[i]) * wordChars.length), 1).join("");
-    let scrimbleDisplay = document.querySelector("#scrimbles");
-    let wordDisplay = document.createElement("h2")
-    wordDisplay.innerText = newWord;
-    scrimbleDisplay.appendChild(wordDisplay);  
+    if(newPlayer.time > 0) {
+      wordChars = word.split("");
+      const staticLength = wordChars.length;
+      newWord = [];
+      for(let i = 0; i < staticLength; i++) 
+      newWord += wordChars.splice(Math.floor(Math.random(wordChars[i]) * wordChars.length), 1).join("");
+      let scrimbleDisplay = document.querySelector("#scrimbles");
+      let wordDisplay = document.createElement("h2")
+      wordDisplay.innerText = newWord;
+      scrimbleDisplay.appendChild(wordDisplay);
+    } else {
+      this.gameOver();
+    }
   },
   
   playerAnswer(e) {  
@@ -62,42 +67,49 @@ const game = {
       this.checkMatch(x);
       }
   },
+
   checkMatch(x) {
     let clearAnswer = document.querySelector("#input");
       if(x === word) {
         this.increaseScore();
         this.removeWord();
+        this.chooseNewWordEasy();
         this.checkScore();
         this.checkRound();
-        this.chooseNewWordEasy();
-        console.log(randomWordArrayEasy);
       } else {
-        console.log("Try Again");
+        // some kind of "wrong answer try again gif loads"
         clearAnswer.value = "";
       }
   },
+
   increaseScore() {
     newPlayer.score +=1;
     score.innerText = (`Score: ${newPlayer.score}`);
   },
+
+  removeWord() {
+    randomWordArrayEasy.splice(randWord, 1);
+  },
+
   chooseNewWordEasy() {
     let clearAnswer = document.querySelector("#input");
+    clearAnswer.value = "";
     let scrimbleDisplay = document.querySelector("#scrimbles");
     scrimbleDisplay.innerHTML = "";
     randWord = Math.floor(Math.random() * randomWordArrayEasy.length);
     word = randomWordArrayEasy[randWord];
     this.shuffleWord(word);
-    clearAnswer.value = "";
 },
+
   checkScore() {
     if(newPlayer.score === 3) {
       newPlayer.score = 0;
       score.innerText = (`Score: ${newPlayer.score}`);
       newPlayer.round +=1;
       round.innerText = (`Rounds Won: ${newPlayer.round}`);
-      // run roundWon()
     }
   },
+
   checkRound() {
     if(newPlayer.round === 3) {
       newPlayer.game +=1;
@@ -108,19 +120,29 @@ const game = {
       this.gameWon();
     }
   },
-  removeWord() {
-    randomWordArrayEasy.splice(randWord, 1);
-  },
+
   gameWon() {
     if(newPlayer.game === 1) {
       alert("Game Over!");
       location.reload();
     }
   },
-  roundWon() {
-    if(newPlayer.score === 3 && newPlayer.round === 1) {
-      
-    }
+
+  clock() {
+    setInterval( () => {
+      if(newPlayer.time === 0) {
+        this.gameOver();
+      } else {
+          newPlayer.time -=1;
+          const timer = document.querySelector("#time");
+          timer.innerText = (`Time Remaining: ${newPlayer.time}`);
+        }
+    }, 1000);
+  },
+
+  gameOver() {
+    console.log("GAME OVER!");
+    // play again and reload
   }
 }
 document.querySelector("#input").addEventListener("keydown", game.playerAnswer.bind(game))
--- js/arrays.js
+++ js/arrays.js
@@ -1,5 +1,1 @@
 const randomWordArrayEasy = ["chippy", "bummer", "hello", "awesome", "radical", "amazing", "horse", "good", "great", "blatant"];
-
-const randomWordArrayMedium = ["entertainment", "question", "zeitgeist", "enumerate"];
-
-const randomWordArrayHard = [];
